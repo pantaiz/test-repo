2183817b47b94bd827ca688b152a482b06678f36
ivankupryianau <ivankupryianau@gmail.com>
2023-02-07 22:15:51 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type FilterType = 'all' | 'Dollars' | 'RUBLS'
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890', id: 1},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890', id: 2},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890', id: 3},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890', id: 4},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890', id: 5},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890', id: 6},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890', id: 7},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890', id: 8},
+    ])
+
+    const [filter, setFilter] = useState<FilterType>('all')
+
+    console.log(filter)
+
+    let usdCurrency = money
+
+    if (filter === 'Dollars') {
+        usdCurrency = money.filter(t => t.banknots === 'Dollars')
+    }
+    if (filter === 'RUBLS') {
+        usdCurrency = money.filter(t => t.banknots === 'RUBLS')
+    }
+
+    const onClickFilterHandler = (btnTitle: FilterType) => {
+        setFilter('Dollars')
+        console.log(`state is ${filter}`)
+    }
+
+    return (
+        <>
+            <ul>
+                {usdCurrency.map((t, index) => {
+                    return (
+                        <li key={t.id}>
+                            <span> {t.banknots}</span>
+                            <span> {t.value}</span>
+                            <span> {t.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft: '50px'}}>
+                <button onClick={() => onClickFilterHandler('all')}>All</button>
+                <button onClick={() => onClickFilterHandler('Dollars')}>Dollars</button>
+                <button onClick={() => onClickFilterHandler('RUBLS')}>RUBLS</button>
+            </div>
+        </>
+    );
 }
 
 export default App;
